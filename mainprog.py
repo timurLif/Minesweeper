fb50510ee64a530c38bb3c3ce7cfba9fd43a6d99
timurLif <tmmmllw@gmail.com>
2024-02-28 15:59:40 +0300

    Изменения в дизайне

--- mainprog.py
+++ mainprog.py
@@ -4,7 +4,7 @@ import sys
 import pygame
 import random
 
-
+# Функция загрузки фото
 def load_image(name, colorkey=None):
     fullname = os.path.join('pictures', name)
     if not os.path.isfile(fullname):
@@ -26,6 +26,7 @@ class Board:
         self.lobby_button = False
         self.changes_on_the_board = True
         self.count_open_cells = 0
+        self.result = 'run'
 
         list_mines = []
         if field_width == field_height == 8:
@@ -57,6 +58,7 @@ class Board:
 
         self.creation_of_numbers()
 
+    # Создание чисел в зависимости от количество рядом располженых бомб
     def creation_of_numbers(self):
         for y in range(self.field_height):
             for x in range(self.field_width):
@@ -83,12 +85,13 @@ class Board:
                     if count != 0:
                         self.board[y][x] = f'-{count}'
 
-    # настройка внешнего вида
+    # настройка внешнего вида поля
     def set_view(self, left, top, cell_size):
         self.left = left
         self.top = top
         self.cell_size = cell_size
 
+    # Проверка нажатия в определенных зонах
     def get_click(self, mouse_pos):
         cell = self.get_cell(mouse_pos)
         if cell != None and self.changes_on_the_board == True:
@@ -106,6 +109,7 @@ class Board:
     def lobby(self):
         return self.lobby_button
 
+    # Открытие клеток
     def open_cell(self, x, y):
         if self.board[y][x] == '-':
             self.board[y][x] = '0'
@@ -188,13 +192,14 @@ class Board:
 
     def time(self):
         font_time = pygame.font.Font(None, 50)
-        minutes = time_elapsed // 60000
-        seconds = (time_elapsed % 60000) // 1000
-        text_time = font_time.render(f'{minutes:02d}:{seconds:02d}', True, 'white')
+
+        if self.result == 'run':
+            self.minutes = time_elapsed // 60000
+            self.seconds = (time_elapsed % 60000) // 1000
+        text_time = font_time.render(f'{self.minutes:02d}:{self.seconds:02d}', True, 'white')
         screen.blit(text_time, (475, 125))
         board_sprites.draw(screen)
 
-        # restart и выход в меню
         pygame.draw.rect(screen, 'white', (445, 200, 137, 90), 2)
         pygame.draw.rect(screen, 'white', (445, 340, 137, 90), 2)
 
@@ -221,18 +226,20 @@ class Board:
                     self.time()
 
                 if self.changes_on_the_board == False and self.board[y][x] == '*':
+                    self.result = 'loss'
                     screen.fill(pygame.Color('red'), pygame.Rect(x * self.cell_size + self.left + 1,
                                                                  y * self.cell_size + self.top + 1,
                                                                  self.cell_size - 2, self.cell_size - 2))
                     self.time()
 
                 if self.changes_on_the_board == 'win' and self.board[y][x] == '*':
+                    self.result = 'win'
                     screen.fill(pygame.Color('green'), pygame.Rect(x * self.cell_size + self.left + 1,
                                                                    y * self.cell_size + self.top + 1,
                                                                    self.cell_size - 2, self.cell_size - 2))
+                
 
-
-# Пробный класс главного меню, который надо переделать и сделать красивым
+# Реализация главного меню
 class MainMenu:
     def __init__(self):
         self.game_mode = (0, 0)
@@ -241,14 +248,14 @@ class MainMenu:
 
     def get_click(self, mouse_pos):
         if self.current_screen == 'start':
-            if 100 < mouse_pos[0] < 200 and 100 < mouse_pos[1] < 150:
+            if 50 < mouse_pos[0] < 250 and 100 < mouse_pos[1] < 175:
                 self.current_screen = 'select_screen'
 
         elif self.current_screen == 'select_screen':
-            if 50 < mouse_pos[0] < 150 and 100 < mouse_pos[1] < 200:
+            if 50 < mouse_pos[0] < 250 and 100 < mouse_pos[1] < 300:
                 self.game_mode = (8, 8)
 
-            elif 250 < mouse_pos[0] < 350 and 100 < mouse_pos[1] < 200:
+            elif 350 < mouse_pos[0] < 550 and 100 < mouse_pos[1] < 300:
                 self.game_mode = (16, 16)
             self.count = 'New board'
 
@@ -260,26 +267,25 @@ class MainMenu:
             return (False, self.game_mode)
 
     def render(self, screen):
-        font = pygame.font.Font(None, 35)
+        font_start = pygame.font.Font(None, 45)
+        font_choice = pygame.font.Font(None, 65)
         if self.current_screen == 'select_screen':
-            pygame.draw.rect(screen, 'black', (50, 45, 400, 35))
-            text = font.render(f'Выбери режим: 8 * 8 или 16 * 16', True, 'white')
-            screen.blit(text, (50, 50))
+            pygame.draw.rect(screen, 'black', (50, 100, 200, 200))
+            pygame.draw.rect(screen, 'white', (50, 100, 200, 200), 2)
+            text_1 = font_choice.render(f'8 * 8', True, 'white')
+            screen.blit(text_1, (105, 170))
 
-            pygame.draw.rect(screen, 'black', (50, 100, 100, 100))
-            text_1 = font.render(f'8 * 8', True, 'white')
-            screen.blit(text_1, (76, 135))
-
-            pygame.draw.rect(screen, 'black', (250, 100, 100, 100))
-            text_2 = font.render(f'16 * 16', True, 'white')
-            screen.blit(text_2, (263, 135))
+            pygame.draw.rect(screen, 'black', (350, 100, 200, 200))
+            pygame.draw.rect(screen, 'white', (350, 100, 200, 200), 2)
+            text_2 = font_choice.render(f'16 * 16', True, 'white')
+            screen.blit(text_2, (380, 170))
 
 
         elif self.current_screen == 'start':
-            pygame.draw.rect(screen, 'black', (100, 100, 100, 50))
-            text_0 = font.render(f'Играть', True, 'white')
-            screen.blit(text_0, (112, 115))
-
+            pygame.draw.rect(screen, 'black', (50, 100, 200, 75))
+            pygame.draw.rect(screen, 'white', (50, 100, 200, 75), 2)
+            text_0 = font_start.render(f'START', True, 'white')
+            screen.blit(text_0, (105, 127))
 
 
 if __name__ == '__main__':
@@ -288,7 +294,7 @@ if __name__ == '__main__':
     size = width, height = 600, 470
     screen = pygame.display.set_mode(size)
 
-    # Спрайты в классе Board
+    # Спрайты класса Board
     board_sprites = pygame.sprite.Group()
 
     clock_sprite = pygame.sprite.Sprite(board_sprites)
@@ -312,12 +318,20 @@ if __name__ == '__main__':
     lobby_sprite.rect.x = 463
     lobby_sprite.rect.y = 333
 
+    # Спрайты класса MainMenu
+    bg_sprites = pygame.sprite.Group()
+
+    bg = pygame.sprite.Sprite(bg_sprites)
+    bg.image = load_image("naval_mine.jpeg")
+    bg.rect = bg.image.get_rect()
+    bg.image = pygame.transform.scale(bg.image, (900, 500))
+    bg.rect.x = 0
+    bg.rect.y = 0
+
     fps = 60
     clock = pygame.time.Clock()
-    # board = Board(8, 8)
     main_menu = MainMenu()
     game_run = False
-    # board.set_view(100, 50, 40)
 
     running = True
     while running:
@@ -332,9 +346,10 @@ if __name__ == '__main__':
                     main_menu.get_click(event.pos)
 
         screen.fill((0, 0, 0))
+
+        # Отображение и нужных окон в зависимости от ситуации и выбора
         if not game_run:
-            bg = load_image("minesweeper.jpeg")
-            screen.blit(bg, (-20, -15))
+            bg_sprites.draw(screen)
         if main_menu.open_game()[0]:
             main_menu.render(screen)
         elif main_menu.open_game()[0] is False and main_menu.count == 'New board':
